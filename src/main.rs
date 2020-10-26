64c9c0a522f5c12b6ef9ad8966c0062e58ca06ba
codermarcos <coder.marcos@gmail.com>
2020-10-26 02:48:49 -0300

    :poop: temp improve logs to see results

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@
 use rust_simple_parser::parse;
 
 fn main() {
+	println!("-----------------------------------------");
+
 	println!("{:#?}", parse(String::from("Olá Marcos")));
+
+	println!("-----------------------------------------");
+
 	println!("{:#?}", parse(String::from("<h1>Olá Marcos</h1>")));
+
+	println!("-----------------------------------------");
+
 	println!("{:#?}", parse(String::from("<h1><b>Olá Marcos</b></h1>")));
+
+	println!("-----------------------------------------");
+
 	println!("{:#?}", parse(String::from("<h1>Olá <b>Marcos</b></h1>")));
+
+	println!("-----------------------------------------");
+
 	println!("{:#?}", parse(String::from("<header><h1>Olá <b>Marcos</b></h1><h2>Sou Frontend</h2></header>")));
+
+	println!("-----------------------------------------");
 }
