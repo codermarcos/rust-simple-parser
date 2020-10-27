7dc8d1857423699bee5315f982006fe0df7de8bb
codermarcos <coder.marcos@gmail.com>
2020-10-27 19:43:19 -0300

    :poop: add example to call

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,33 @@
 use rust_simple_parser::parse;
 
 fn main() {
-	println!("-----------------------------------------");
+	println!("simple text ---------------------------------------");
 
 	println!("{:#?}", parse(String::from("Olá Marcos")));
 
-	println!("-----------------------------------------");
+	println!("tag close -----------------------------------------");
 
 	println!("{:#?}", parse(String::from("<h1>Olá Marcos</h1>")));
 
-	println!("-----------------------------------------");
+	println!("child tags ----------------------------------------");
 
 	println!("{:#?}", parse(String::from("<h1><b>Olá Marcos</b></h1>")));
 
-	println!("-----------------------------------------");
+	println!("child tags with text ------------------------------");
 
 	println!("{:#?}", parse(String::from("<h1>Olá <b>Marcos</b></h1>")));
 
-	println!("-----------------------------------------");
-
+	println!("self close tags -----------------------------------");
 
 	println!("{:#?}", parse(String::from("<form><input/></form>")));
 
-	println!("-----------------------------------------");
+	println!("siblings tags -------------------------------------");
 
 	println!("{:#?}", parse(String::from("<header><h1>Olá <b>Marcos</b></h1><h2>Sou Frontend</h2></header>")));
+
+	println!("tag with attributes -------------------------------");
+
+	println!("{:#?}", parse(String::from("<input id=\"teste\" />")));
 
 	println!("-----------------------------------------");
 }
