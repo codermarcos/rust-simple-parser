402d03200ee64ec77f8bff9b5ce917e1342babdf
codermarcos <coder.marcos@gmail.com>
2020-12-12 04:24:26 -0300

    :art: start refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,6 @@
 use std::collections::HashMap;
 
 #[derive(PartialEq, Debug)]
-enum Actions {
-	EmptyState,
-	ReadingInnerTag,
-	ReadingAttributes,
-	ReadingOpenTagName,
-	ReadingCloseTagName,
-	ReadingAttributesValue(String),
-}
-
-#[derive(PartialEq, Debug)]
 pub struct HtmlElement {
 	pub node_type: String,
 	pub text_content: String,
@@ -29,186 +19,118 @@ impl HtmlElement {
 	}
 }
 
-const SELF_CLOSE_TAGS: [&str; 14] = [
-	"area",
-	"base",
-	"br", 
-	"col",
-	"embed",
-	"hr",
-	"img",
-	"input",
-	"link",
-	"meta",
-	"param",
-	"source",
-	"track",
-	"wbr",
-];
+#[derive(PartialEq, Debug)]
+enum Tokens {
+	Text(String),
+	OpenTag(String),
+	CloseTag(String),
+	SelfCloseTag(String),
+	AttributeKey(String),
+	AttributeValue(String),
+}
 
-fn parse_shallow(html: String) -> (Vec<HtmlElement>, usize) {
-	let mut parsed: Vec<HtmlElement> = vec![];
-	let mut state: Vec<Actions> = vec![];
-	let mut reading = String::new();
+impl Tokens {
+	fn add(&mut self, c: char) {
+		match *self {
+			Tokens::Text(ref mut s)
+			| Tokens::OpenTag(ref mut s)
+			| Tokens::CloseTag(ref mut s)
+			| Tokens::SelfCloseTag(ref mut s)
+			| Tokens::AttributeKey(ref mut s)
+			| Tokens::AttributeValue(ref mut s) => {
+				*s = String::from(s.clone()) + &String::from(c);
+			}
+		}
+	}
+
+	fn convert_to_self_close_tag(&mut self) {
+		if let Tokens::OpenTag(tag) = &*self {
+			*self = Tokens::SelfCloseTag(tag.clone());
+		}
+	}
+}
+
+fn lexer(html: String) -> Vec<Tokens> {
+	let mut tokens: Vec<Tokens> = vec![];
+	let mut reading = false;
 	let mut idx = 0;
 
 	let to_read = html.clone();
 
 	loop {
-		if let Some(last_state) = state.last().clone() {
-			if let Some(letter) = to_read.chars().nth(idx) {
-				match letter {
-					'<' => {
-						if let Some(next_letter) = to_read.chars().nth(idx + 1) {
-							if next_letter == '/' {
-								state.push(Actions::ReadingCloseTagName);
+		let posible_last_token = tokens.last_mut();
+		let posible_letter = to_read.chars().nth(idx);
+		let posible_next_letter = to_read.chars().nth(idx + 1);
 
-								if reading.capacity() > 0 {
-									if parsed.len() > 0 {
-										parsed[0].text_content = reading.clone(); 
-									} else {
-										let mut element = HtmlElement::new(String::from("text"));
-										element.text_content = reading.clone();
-										parsed.push(element);
-									}
-									
-									reading = String::new();
-								}
-								
-								idx += 2;
-							} else {
-								if last_state == &Actions::ReadingInnerTag {
-									let last_idx_parsed = parsed.len() - 1;
-									
-									if reading.capacity() > 0 {
-										parsed[last_idx_parsed].text_content = reading;
-										reading = String::new();
-										idx += 1;
-									}
-
-									let deep_read = (&to_read[idx..]).to_string(); 
-									let (elements, readed) = parse_shallow(deep_read);
-
-									for element in elements {
-										parsed[last_idx_parsed].child_nodes.push(element);
-									}
-
-									idx += readed;
-								} else {
-									state.push(Actions::ReadingOpenTagName);
-									idx += 1;
-								}
-							}
-						}
-					}
-					'>' => {
-						match last_state {
-							Actions::ReadingOpenTagName => {
-								let element = HtmlElement::new(reading.clone());
-								let tag_name: &str = &reading;
-								parsed.push(element);
-								state.pop();
-
-								if !SELF_CLOSE_TAGS.contains(&tag_name) {
-									state.push(Actions::ReadingInnerTag);
-								}
-
-								reading = String::new();
-							}
-							Actions::ReadingAttributes => {
-								reading = String::new();
-								
-								state.pop();
-								idx += 1;	
-							}
-							Actions::ReadingCloseTagName => {
-								state.pop();
-								break (parsed, idx);
-							}
-							_ => {}
-						}
-
-						idx += 1;
-					}
-					'/' => {
-						if let Some(next_letter) = to_read.chars().nth(idx + 1) {
-							if next_letter == '>' {
-								match last_state {
-									Actions::ReadingOpenTagName => {
-										let element = HtmlElement::new(reading.clone());
-										reading = String::new();
-										parsed.push(element);
-										
-										state.pop();
-										idx += 2;	
-									}
-									Actions::ReadingAttributes => {
-										reading = String::new();
-										
-										state.pop();
-										idx += 1;	
-									}
-									_ => {}
-								}								
-							}
-						}
-					}
-					'=' | '"' => {	
-						match last_state {
-							Actions::ReadingAttributes => {							
-								if reading.capacity() > 0 {
-									state.push(Actions::ReadingAttributesValue(reading));
-									reading = String::new();
-								}
-							}
-							Actions::ReadingAttributesValue(key) => {
-								let last_idx_parsed = parsed.len() - 1;
-									
-								if reading.capacity() > 0 {
-									parsed[last_idx_parsed].attributes.insert(key.to_string(), Some(reading.clone()));
-									reading = String::new();
-									state.pop();
-								}
-							}
-							_ => {
-								reading = reading + letter.to_string().as_str();
-							}
-						}
-						idx += 1;
-					}
-					_ => {
-						match last_state {
-							Actions::ReadingOpenTagName => {
-								if letter == ' ' {
-									state.pop();
-									parsed.push(HtmlElement::new(reading.clone()));
-									state.push(Actions::ReadingAttributes);
-									reading = String::new();
-								} else {
-									reading = reading + letter.to_string().as_str();
-								}
-							}
-							_ => {
-								reading = reading + letter.to_string().as_str();
-							}
-						}
-
-						idx += 1;
-					}
-				}
-			} else {
-				if reading.capacity() > 0 {
-					let mut element = HtmlElement::new(String::from("text"));
-					element.text_content = reading.clone();
-					parsed.push(element);
-				}
-
-				break (parsed, idx);
+		match (posible_letter, posible_next_letter, posible_last_token) {
+			(None, ..) => {
+				break tokens;
 			}
-		} else {
-			state.push(Actions::EmptyState);
+			(Some('<'), Some(next_letter), _) => {
+				let token = if next_letter == '/' {
+					Tokens::CloseTag(String::new())
+				} else {
+					Tokens::OpenTag(String::from(next_letter))
+				};
+
+				tokens.push(token);
+				reading = true;
+				idx += 2;
+			}
+			(Some('/'), Some('>'), _) => {
+				if let Some(token) = tokens.iter_mut().rev().find(|i| match i {
+					Tokens::OpenTag(_) => true,
+					_ => false,
+				}) {
+					token.convert_to_self_close_tag();
+					reading = false;
+				}
+
+				idx += 2;
+			}
+			(Some('>'), ..) | (Some('"'), ..) => {
+				reading = false;
+				idx += 1;
+			}
+			(Some('='), Some('"'), _) => {
+				tokens.push(Tokens::AttributeValue(String::new()));
+				reading = true;
+				idx += 2;
+			}
+			(Some(' '), Some('/'), _) => {
+				reading = false;
+				idx +=2;
+			}
+			(Some(' '), _, Some(last_token @ Tokens::AttributeValue(_))) if reading => {
+				last_token.add(' ');
+				idx += 1;
+			}
+			(Some(' '), Some(next_letter), Some(Tokens::OpenTag(_)))
+			| (Some(' '), Some(next_letter), Some(Tokens::AttributeValue(_)))
+				if next_letter != '/' && next_letter != ' ' =>
+			{
+				tokens.push(Tokens::AttributeKey(String::new()));
+				reading = true;
+				idx += 1;
+			}
+			(Some(letter), _, _) if !reading => {
+				tokens.push(Tokens::Text(String::from(letter)));
+				reading = true;
+				idx += 1;
+			}
+			(Some(letter), _, Some(last_token)) => {
+				last_token.add(letter);
+				idx += 1;
+			}
+			_ => {
+
+			}
 		}
 	}
+}
+
+fn parser(_tokens: Vec<Tokens>) -> Vec<HtmlElement> {
+	vec![]
 }
 
 /// Returns a parsed html with the value given them
@@ -223,10 +145,10 @@ fn parse_shallow(html: String) -> (Vec<HtmlElement>, usize) {
 /// use crate::rust_simple_parser::HtmlElement;
 /// use crate::rust_simple_parser::parse;
 /// use std::collections::HashMap;
-/// 
+///
 ///	let html = String::from("<h1>Olá Marcos</h1>");
 ///	let parsed = parse(html);
-/// 
+///
 ///	let expected: Vec<HtmlElement> = vec![
 ///		HtmlElement {
 ///			text_content: String::from("Olá Marcos"),
@@ -235,34 +157,35 @@ fn parse_shallow(html: String) -> (Vec<HtmlElement>, usize) {
 ///			child_nodes: Vec::new(),
 ///		}
 ///	];
-/// 
+///
 /// assert_eq!(expected, parsed);
 /// ```
 pub fn parse(html: String) -> Vec<HtmlElement> {
-	let (parsed, _) = parse_shallow(html);
-	
+	let tokens = lexer(html);
+	println!("{:#?}", tokens);
+
+	let parsed = parser(tokens);
+
 	parsed
 }
 
 #[cfg(test)]
 mod tests {
-	use std::collections::HashMap;
-	use crate::HtmlElement;
 	use crate::parse;
+	use crate::HtmlElement;
+	use std::collections::HashMap;
 
 	#[test]
 	fn parse_text() {
 		let text_content = String::from("Olá Marcos");
 		let lexer = parse(text_content.clone());
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("text"),
-				text_content: text_content,
-				attributes: HashMap::new(),
-				child_nodes: Vec::new(),
-			}
-		];
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("text"),
+			text_content: text_content,
+			attributes: HashMap::new(),
+			child_nodes: Vec::new(),
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -273,14 +196,12 @@ mod tests {
 		let html_h1 = String::from("<h1>") + &text_content + &String::from("</h1>");
 		let lexer = parse(html_h1);
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("h1"),
-				text_content: text_content,
-				attributes: HashMap::new(),
-				child_nodes: Vec::new(),
-			}
-		];
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("h1"),
+			text_content: text_content,
+			attributes: HashMap::new(),
+			child_nodes: Vec::new(),
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -291,21 +212,17 @@ mod tests {
 		let html_h1_b = String::from("<h1><b>") + &text_content + &String::from("</b></h1>");
 		let lexer = parse(html_h1_b);
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("h1"),
-				text_content: String::new(),
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("h1"),
+			text_content: String::new(),
+			attributes: HashMap::new(),
+			child_nodes: vec![HtmlElement {
+				node_type: String::from("b"),
+				text_content: text_content,
 				attributes: HashMap::new(),
-				child_nodes: vec![
-					HtmlElement {
-						node_type: String::from("b"),
-						text_content: text_content,
-						attributes: HashMap::new(),
-						child_nodes: Vec::new(),
-					}		
-				],
-			}
-		];
+				child_nodes: Vec::new(),
+			}],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -318,21 +235,17 @@ mod tests {
 		let html_h1_b = String::from("<h1>") + &text_content_h1 + &html_b + &String::from("</h1>");
 		let lexer = parse(html_h1_b);
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("h1"),
-				text_content: text_content_h1,
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("h1"),
+			text_content: text_content_h1,
+			attributes: HashMap::new(),
+			child_nodes: vec![HtmlElement {
+				node_type: String::from("b"),
+				text_content: text_content_b,
 				attributes: HashMap::new(),
-				child_nodes: vec![
-					HtmlElement {
-						node_type: String::from("b"),
-						text_content: text_content_b,
-						attributes: HashMap::new(),
-						child_nodes: Vec::new(),
-					}		
-				],
-			}
-		];
+				child_nodes: Vec::new(),
+			}],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -342,21 +255,17 @@ mod tests {
 		let html_form = String::from("<form><input /></form>");
 		let lexer = parse(html_form);
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("form"),
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("form"),
+			text_content: String::new(),
+			attributes: HashMap::new(),
+			child_nodes: vec![HtmlElement {
+				node_type: String::from("input"),
 				text_content: String::new(),
 				attributes: HashMap::new(),
-				child_nodes: vec![
-					HtmlElement {
-						node_type: String::from("input"),
-						text_content: String::new(),
-						attributes: HashMap::new(),
-						child_nodes: Vec::new(),
-					},
-				],
-			}
-		];
+				child_nodes: Vec::new(),
+			}],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -370,14 +279,12 @@ mod tests {
 
 		attributes.insert("id".to_string(), Some(String::from("teste")));
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("input"),
-				text_content: String::new(),
-				attributes: attributes,
-				child_nodes: vec![],
-			}
-		];
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("input"),
+			text_content: String::new(),
+			attributes: attributes,
+			child_nodes: vec![],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -391,14 +298,12 @@ mod tests {
 
 		attributes.insert("id".to_string(), Some(String::from("teste")));
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("input"),
-				text_content: String::new(),
-				attributes: attributes,
-				child_nodes: vec![],
-			}
-		];
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("input"),
+			text_content: String::new(),
+			attributes: attributes,
+			child_nodes: vec![],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
@@ -410,38 +315,35 @@ mod tests {
 		let text_content_h2 = String::from("Sou Frontend");
 		let html_b = String::from("<b>") + &text_content_b + &String::from("</b>");
 		let html_h1_b = String::from("<h1>") + &text_content_h1 + &html_b + &String::from("</h1>");
-		let html_h1_b_h2 = html_h1_b + &String::from("<h2>") + &text_content_h2 + &String::from("</h2>");
+		let html_h1_b_h2 =
+			html_h1_b + &String::from("<h2>") + &text_content_h2 + &String::from("</h2>");
 		let html_header = String::from("<header>") + &html_h1_b_h2 + &String::from("</header>");
 		let lexer = parse(html_header);
 
-		let expected: Vec<HtmlElement> = vec![
-			HtmlElement {
-				node_type: String::from("header"),
-				text_content: String::new(),
-				attributes: HashMap::new(),
-				child_nodes: vec![
-					HtmlElement {
-						node_type: String::from("h1"),
-						text_content: text_content_h1,
-						attributes: HashMap::new(),
-						child_nodes: vec![
-							HtmlElement {
-								node_type: String::from("b"),
-								text_content: text_content_b,
-								attributes: HashMap::new(),
-								child_nodes: Vec::new(),
-							},		
-						],
-					},
-					HtmlElement {
-						node_type: String::from("h2"),
-						text_content: text_content_h2,
+		let expected: Vec<HtmlElement> = vec![HtmlElement {
+			node_type: String::from("header"),
+			text_content: String::new(),
+			attributes: HashMap::new(),
+			child_nodes: vec![
+				HtmlElement {
+					node_type: String::from("h1"),
+					text_content: text_content_h1,
+					attributes: HashMap::new(),
+					child_nodes: vec![HtmlElement {
+						node_type: String::from("b"),
+						text_content: text_content_b,
 						attributes: HashMap::new(),
 						child_nodes: Vec::new(),
-					},
-				],
-			}
-		];
+					}],
+				},
+				HtmlElement {
+					node_type: String::from("h2"),
+					text_content: text_content_h2,
+					attributes: HashMap::new(),
+					child_nodes: Vec::new(),
+				},
+			],
+		}];
 
 		assert_eq!(expected, lexer);
 	}
--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,45 @@
 use rust_simple_parser::parse;
 
 fn main() {
-	println!("simple text ---------------------------------------");
+	// println!("simple text ---------------------------------------");
 
-	println!("{:#?}", parse(String::from("Olá Marcos")));
+	// println!("{:#?}", parse(String::from("Olá Marcos")));
 
-	println!("tag close -----------------------------------------");
+	// println!("tag close -----------------------------------------");
 
-	println!("{:#?}", parse(String::from("<h1>Olá Marcos</h1>")));
+	// println!("{:#?}", parse(String::from("<h1>Olá Marcos</h1>")));
 
-	println!("child tags ----------------------------------------");
+	// println!("child tags ----------------------------------------");
 
-	println!("{:#?}", parse(String::from("<h1><b>Olá Marcos</b></h1>")));
+	// println!("{:#?}", parse(String::from("<h1><b>Olá Marcos</b></h1>")));
 
-	println!("child tags with text ------------------------------");
+	// println!("child tags with text ------------------------------");
 
-	println!("{:#?}", parse(String::from("<h1>Olá <b>Marcos</b></h1>")));
+	// println!("{:#?}", parse(String::from("<h1>Olá <b>Marcos</b></h1>")));
 
-	println!("self close tags -----------------------------------");
+	// println!("self close tags -----------------------------------");
 
-	println!("{:#?}", parse(String::from("<form><input/></form>")));
+	// println!("{:#?}", parse(String::from("<form><input/></form>")));
 
-	println!("siblings tags -------------------------------------");
+	// println!("siblings tags -------------------------------------");
 
-	println!("{:#?}", parse(String::from("<header><h1>Olá <b>Marcos</b></h1><h2>Sou Frontend</h2></header>")));
+	// println!("{:#?}", parse(String::from("<header><h1>Olá <b>Marcos</b></h1><h2>Sou Frontend</h2></header>")));
 
-	println!("tag with attributes -------------------------------");
+	// println!("tag with attributes -------------------------------");
 
-	println!("{:#?}", parse(String::from("<input id=\"teste\" />")));
+	// println!("{:#?}", parse(String::from("<input id=\"teste\" />")));
 
-	println!("tag with spaced attributes -------------------------");
+	println!("tag with multiple attributes ----------------------");
 
-	println!("{:#?}", parse(String::from("<input class=\"teste teste\" />")));
+	println!("{:#?}", parse(String::from("<input id=\"teste\" class=\"campo\" />")));
 
-	println!("-----------------------------------------");
+	// println!("child self close tag with attributes -------------------------------");
+
+	// println!("{:#?}", parse(String::from("<form>Nome: <input id=\"teste\" /> <button>enviar</button></form>")));
+
+	// println!("tag with spaced attributes -------------------------");
+
+	// println!("{:#?}", parse(String::from("<input class=\"teste teste\" />")));
+
+	// println!("-----------------------------------------");
 }
